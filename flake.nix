239932d69e52e9633ef7d1955f47e432e3ca435a
Jacek Galowicz <jacek@galowicz.de>
2023-11-03 18:08:56 +0100

    Add crosscompilation

--- flake.nix
+++ flake.nix
@@ -9,6 +9,13 @@
     advisory-db.flake = false;
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
@@ -24,8 +31,9 @@
         src = craneLib.cleanCargoSource (craneLib.path ./.);
         cargoArtifacts = craneLib.buildDepsOnly {
           inherit src;
-          buildInputs = [ pkgs.openssl pkgs.pkg-config ];
+          buildInputs = with pkgs; [ openssl pkg-config ];
         };
+
       in
       {
         devShells.default = pkgs.mkShell {
@@ -41,6 +49,64 @@
           docs = craneLib.cargoDoc {
             inherit cargoArtifacts src;
           };
+
+          static =
+            let
+              staticPkgs = import inputs.nixpkgs {
+                inherit system;
+                overlays = [ (import inputs.rust-overlay) ];
+              };
+
+              archPrefix = builtins.elemAt (pkgs.lib.strings.split "-" system) 0;
+
+              staticCraneLib =
+                let
+                  rustToolchain = staticPkgs.rust-bin.stable.latest.default.override {
+                    targets = [ "${archPrefix}-unknown-linux-musl" ];
+                  };
+                in
+                  (inputs.crane.mkLib staticPkgs).overrideToolchain rustToolchain;
+
+            in
+              staticCraneLib.buildPackage {
+                inherit src;
+
+                CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+                CARGO_BUILD_RUSTFLAGS = "-C target-feature=+crt-static";
+
+                nativeBuildInputs = [ pkgs.pkg-config ];
+                buildInputs = [ pkgs.pkgsStatic.openssl ];
+              };
+        } // pkgs.lib.optionalAttrs (!pkgs.stdenv.isAarch64) {
+          crossArm =
+            let
+              crossSystem = "aarch64-linux";
+
+              crossPkgs = import inputs.nixpkgs {
+                inherit crossSystem;
+                localSystem = system;
+                overlays = [ (import inputs.rust-overlay) ];
+              };
+
+              rustToolchain = crossPkgs.pkgsBuildHost.rust-bin.stable.latest.default.override {
+                targets = [ "aarch64-unknown-linux-gnu" ];
+              };
+
+              craneLib = (inputs.crane.mkLib crossPkgs).overrideToolchain rustToolchain;
+
+              crateExpression = { openssl , lib , pkg-config , stdenv }:
+                craneLib.buildPackage {
+                  inherit src;
+
+                  nativeBuildInputs = [ pkg-config ];
+                  buildInputs = [ openssl ];
+
+                  CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER = "${stdenv.cc.targetPrefix}cc";
+                  CARGO_BUILD_TARGET = "aarch64-unknown-linux-gnu";
+                  HOST_CC = "${stdenv.cc.nativePrefix}cc";
+                };
+            in
+              crossPkgs.callPackage crateExpression { };
         };
 
         checks = {
